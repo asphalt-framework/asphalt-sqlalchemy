fc8bbd93befa72a9875fe85b61e32a83864991be
Alex Grönholm <alex.gronholm@nextday.fi>
2017-10-01 11:30:47 +0300

    Updated packaging/testing/documentation configuration

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.5.2
 
-RUN wget -qO- https://github.com/jwilder/dockerize/releases/download/v0.4.0/dockerize-linux-amd64-v0.4.0.tar.gz |\
+RUN wget -qO- https://github.com/jwilder/dockerize/releases/download/v0.5.0/dockerize-linux-amd64-v0.5.0.tar.gz |\
     tar xzC /usr/local/bin
 
 # Required for CyMySQL
@@ -8,7 +8,9 @@ RUN adduser -u 1000 testuser
 
 ENV SETUPTOOLS_SCM_PRETEND_VERSION 2.0.0
 
+RUN pip install -U setuptools
+
 WORKDIR /app
 COPY asphalt ./asphalt
 COPY setup.* README.rst ./
-RUN pip install -e .[testing]
+RUN pip install -e .[test]
--- setup.py
+++ setup.py
@@ -1,53 +1,12 @@
-from pathlib import Path
-
 from setuptools import setup
 
 setup(
-    name='asphalt-sqlalchemy',
     use_scm_version={
         'version_scheme': 'post-release',
         'local_scheme': 'dirty-tag'
     },
-    description='SQLAlchemy integration component for the Asphalt framework',
-    long_description=Path(__file__).with_name('README.rst').read_text('utf-8'),
-    author='Alex Grönholm',
-    author_email='alex.gronholm@nextday.fi',
-    url='https://github.com/asphalt-framework/asphalt-sqlalchemy',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Topic :: Database',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-    license='Apache License 2.0',
-    zip_safe=False,
-    packages=[
-        'asphalt.sqlalchemy'
-    ],
     setup_requires=[
+        'setuptools >= 36.2.7',
         'setuptools_scm >= 1.7.0'
-    ],
-    install_requires=[
-        'asphalt >= 3.0, < 5.0',
-        'SQLAlchemy >= 1.0.10'
-    ],
-    extras_require={
-        'testing': [
-            'pytest',
-            'pytest-asyncio >= 0.6',
-            'pytest-catchlog',
-            'pytest-cov',
-            'psycopg2',
-            'cymysql'
-        ]
-    },
-    entry_points={
-        'asphalt.components': [
-            'sqlalchemy = asphalt.sqlalchemy.component:SQLAlchemyComponent'
-        ]
-    }
+    ]
 )
