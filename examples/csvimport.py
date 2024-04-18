c2d8b9c79ebbeb16c4fd1b9197ec09590d018fda
Alex Grönholm <alex.gronholm@nextday.fi>
2024-04-19 01:32:16 +0300

    Updated use of run_application() in examples to match the latest changes

--- examples/csvimport.py
+++ examples/csvimport.py
@@ -9,7 +9,6 @@ import csv
 import logging
 from pathlib import Path
 
-import anyio
 from anyio import to_thread
 from asphalt.core import (
     CLIApplicationComponent,
@@ -72,4 +71,4 @@ class CSVImporterComponent(CLIApplicationComponent):
         logger.info("Imported %d rows of data", inserted_rows)
 
 
-anyio.run(run_application, CSVImporterComponent())
+run_application(CSVImporterComponent())
